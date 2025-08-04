90d0d88e4b42746f63598f018d807b2b61f034c4
47-5 <1197748182@qq.com>
2025-08-04 23:04:19 +0800

    make package

--- QMkinetics/__init__.py
+++ QMkinetics/__init__.py
@@ -1,3 +1,5 @@
 from QMkinetics.equilibrium_constants import *
 from QMkinetics.qm_kinetics import *
 from QMkinetics.tunnelling_effect import *
+from QMkinetics.fit_kinetics import *
+from QMkinetics.export_cantera_kinetics_yaml import *
